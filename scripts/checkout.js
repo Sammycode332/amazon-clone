b94f51981db4ad57ca59ba764cc73b66a166009f
SammyCode 332 <samueloroja123@gmail.com>
2025-11-04 20:47:16 +0100

    worked on importing external libaries

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,12 @@
 import { cart, removeFromCart,calculateCartQuantity,loadCartFromStorage, saveToStorage } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";  
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
+const today  = dayjs()
+const deliveryDate = today.add(7,'days')
+console.log(deliveryDate.format('dddd, MMMM D'))
+ 
 loadCartFromStorage();
 let cartSummaryHtml = "";
 cart.forEach((cartItem)=>{
@@ -9,7 +14,7 @@ cart.forEach((cartItem)=>{
   const productId = cartItem.productId;
   let matchingProduct;
 
-  products.forEach((product)=>{
+  products.forEach((product)=>{ 
     if(product.id === productId){
       matchingProduct = product;
     }
