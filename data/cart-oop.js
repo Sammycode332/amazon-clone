5c98798f218a7c086dc601e7333710462fa401d1
SammyCode 332 <samueloroja123@gmail.com>
2025-11-15 12:18:46 +0100

    Create oop version of cart

---
+++ data/cart-oop.js
@@ -0,0 +1,67 @@
+import { products } from './products.js';
+// when creating function that re object use pascal case
+
+function Cart(localStorageKey){
+    const cart = {
+  cartItems : JSON.parse(localStorage.getItem(localStorageKey)) ||[{
+  deliveryOptionId:'1',
+},{
+  deliveryOptionId:'2'
+},{
+  deliveryOptionId:'3'
+}
+],
+ loadCartFromStorage() {
+  // re-read storage and update exported binding
+  this.cartItems = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+  return this.cartItems;
+},
+saveToStorage(){
+  localStorage.setItem(localStorageKey,JSON.stringify(this.cartItems))
+},
+ calculateCartQuantity(){
+  let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+
+     
+    });
+    return cartQuantity;
+ },
+   updateCartQuantity(){
+      const cartQuantity = calculateCartQuantity();
+      document.querySelector(".js-carts-quantity").innerHTML = `Checkout(${cartQuantity} items)`;
+    },
+ removeFromCart(productId){
+  const newCart = [];
+  this.cartItems.forEach((cartItem) =>{
+    if(cartItem.productId !==productId){
+      newCart.push(cartItem)
+    }
+  });
+  this.cartItems = newCart 
+
+  this.saveToStorage();
+},
+
+  updateDeliveryOption(productId, deliveryOptionId){
+    let matchingItem; 
+    this.cartItems.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+    matchingItem.deliveryOptionId  = deliveryOptionId;
+
+    this.saveToStorage();
+ } 
+};
+  return cart;
+}
+
+const cart = Cart('cart-oop');
+const buisnessCart = Cart('cart-business');
+console.log(cart)
+console.log(buisnessCart)
+
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { updateCartQuantity } from "../data/cart.js";
+import '../data/cart-oop.js';
 renderOrderSummary();
 
 renderPaymentSummary()
