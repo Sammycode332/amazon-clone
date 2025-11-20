fdf2249352962b1f1fe243de918b3870dd6079d1
SammyCode 332 <samueloroja123@gmail.com>
2025-11-20 16:00:46 +0100

    Add backend practice file

---
+++ data/backend-practice.js
@@ -0,0 +1,9 @@
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load', ()=>{
+    console.log('Response from backend:', xhr.response)
+});
+xhr.open('GET','https://supersimplebackend.dev') 
+// a static methos to collect sth from the backend
+xhr.open('GET','https://supersimplebackend.dev/products/first') 
+xhr.response    
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,8 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { updateCartQuantity } from "../data/cart.js";
 import '../data/car.js';
+import '../data/backend-practice.js';
+
 renderOrderSummary();
 
 renderPaymentSummary()
