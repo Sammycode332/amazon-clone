37b47448863bcce556e11ac0df8aaf19ed320439
SammyCode 332 <samueloroja123@gmail.com>
2025-11-26 18:46:18 +0100

    updated the promiseAll function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,16 +4,14 @@ import { updateCartQuantity,loadCart } from "../data/cart.js";
 import { loadProducts,loadProductsFetch} from "../data/products.js";
 //import '../data/car.js';
 //import '../data/backend-practice.js';
-Promise.all([
-    new Promise((resolve)=>{
-   loadProductsFetch()
-    //loadProducts(()=>{
+ //loadProducts(()=>{
        
       //resolve('value1');
       //resolve controls when we want to go the the next step
     //});
-    //whatever value you give to result wil be saved in that parameter
-}),
+    //whatever value you give to result wil be saved in that parameter,
+Promise.all([
+   loadProductsFetch(),
    new Promise((resolve)=>{
       loadCart(()=>{
         resolve();
@@ -42,7 +40,7 @@ new Promise((resolve)=>{
    }).then(()=>{
         renderOrderSummary();
         renderPaymentSummary();
-    });
+    }); 
     
     //Promise.all() makes us run multiple promises nd wait for them to finish
 */
